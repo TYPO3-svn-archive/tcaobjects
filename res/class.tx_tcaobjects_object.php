897a429a6fdb0a7b38cfe175a8a9499bc9ab79ee
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-02 19:31:10 +0000

    Added some comments
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tcaobjects/trunk@31876 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/class.tx_tcaobjects_object.php
+++ res/class.tx_tcaobjects_object.php
@@ -1,26 +1,27 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2008 Fabrizio Branca (mail@fabrizio-branca.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2008-2010 Fabrizio Branca (mail@fabrizio-branca.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
 
 require_once PATH_t3lib . 'class.t3lib_userauthgroup.php';
 require_once PATH_t3lib . 'class.t3lib_befunc.php';
@@ -30,12 +31,9 @@ require_once PATH_t3lib . 'class.t3lib_befunc.php';
  * Abstract class tx_tcaobjects_object
  * This is the case class for all tcaobjects
  *
- * $Id: class.tx_tcaobjects_object.php,v 1.41 2008/05/12 13:59:36 ry44 Exp $
- *
  * @author	Fabrizio Branca <mail@fabrizio-branca.de>
  * @since	2008-03-20
  */
-
 abstract class tx_tcaobjects_object implements ArrayAccess, IteratorAggregate {
     /**
      * @var string	Name of the table. If empty $this->getClassName() will be used
--- res/class.tx_tcaobjects_objectCollection.php
+++ res/class.tx_tcaobjects_objectCollection.php
@@ -1,6 +1,32 @@
 <?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2008-2010 Fabrizio Branca (mail@fabrizio-branca.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
-// TODO: switch to tx_pttools_iPageable interface?
+/**
+ * Object collection
+ * 
+ * @author Fabrizio Branca <mail@fabrizio-branca.de>
+ */
 class tx_tcaobjects_objectCollection extends tx_pttools_objectCollection implements tx_tcaobjects_iPageable {
 
 	/**
@@ -50,7 +76,7 @@ class tx_tcaobjects_objectCollection extends tx_pttools_objectCollection impleme
 	 * @author	Fabrizio Branca <mail@fabrizio-branca.de>
 	 * @since	2008-06-08
 	 */
-	public function __construct($load = '', array $params = array()) {
+	public function __construct($load='', array $params=array()) {
 
 		// set restricted class name
 		if (is_null($this->restrictedClassName)) {
