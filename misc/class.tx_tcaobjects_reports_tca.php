f169624a258c2a39222bfbb01029df092303efb9
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-28 13:34:34 +0000

    Added tca report (not working until now)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tcaobjects/trunk@33654 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ misc/class.tx_tcaobjects_reports_tca.php
@@ -0,0 +1,155 @@
+<?php 
+
+/**
+ * TCA status provider
+ * 
+ * @author Fabrizio Branca
+ * @since 2010-04-21
+ */
+class tx_tcaobjects_reports_tca implements tx_reports_StatusProvider {
+
+	
+	
+	/**
+	 * Returns the status for this report
+	 *
+	 * @return	array	List of statuses
+	 * @see typo3/sysext/reports/interfaces/tx_reports_StatusProvider::getStatus()
+	 */
+	public function getStatus() {
+		
+		$message = '';
+		foreach ($GLOBALS['TCA'] as $table => $conf) {
+			$message .= $table;
+			
+			// check if all fields used in all pallettes are valid
+			// check if all fields in the pallette are used max once
+			// check if all fields used in all types are valid 
+			// check if all fields in the type are used max once (include check if the field is in a pallette)
+			
+		}
+		
+		
+		if ($true) {
+			$severity = tx_reports_reports_status_Status::OK;
+			$value = 'OK';
+		} else {
+			$severity = tx_reports_reports_status_Status::WARNING;
+			$value = 'WARNING';
+		}
+		
+		$statuses = array(
+			'charsets' => t3lib_div::makeInstance('tx_reports_reports_status_Status',
+				'Character Sets',
+				$value,
+				$message,
+				$severity
+			)
+		);
+
+		return $statuses;
+	}
+	
+	protected function checkTable($table) {
+		t3lib_div::loadTCA($table);
+		
+		$fields = array_keys($GLOBALS['TCA'][$table]['columns']);
+		
+	}
+	
+	/**
+	 * Get table data
+	 * 
+	 * @return array
+	 */
+	protected function getTableData() {
+		$tables = $GLOBALS['TYPO3_DB']->admin_get_tables();
+		foreach ($tables as $table => &$data) {
+			$query = 'SHOW CREATE TABLE ' . $table;
+			$res = $GLOBALS['TYPO3_DB']->admin_query($query);
+			$create = $GLOBALS['TYPO3_DB']->sql_fetch_row($res);
+			$createStatement = $create[1];
+			$matches = array();
+			preg_match('/DEFAULT CHARSET=(\w+)/', $createStatement, $matches);
+			$data['charset'] = $matches[1];
+			
+			$fieldMatches = array();
+			preg_match_all('/`(\w+)`.*character set (\w+)/', $createStatement, $fieldMatches);
+			$data['field_charset'] = array();
+			foreach ($fieldMatches[0] as $key => $fieldMatch) {
+				$data['field_charset'][$fieldMatches[1][$key]] = $fieldMatches[2][$key]; 
+			}
+		}
+		
+		return $tables;
+	}
+	
+	/**
+	 * Check if all tables are in utf8
+	 * 
+	 * @param array $tables
+	 * @return bool
+	 */
+	protected function allUtf8(array $tables) {
+		foreach ($tables as $table => $data) {
+			if ($data['charset'] != 'utf8') {
+				return false;
+			}		
+			foreach ($data['field_charset'] as $field => $charset) {
+				if ($charset != 'utf8') {
+					return false;
+				}	
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Render message
+	 * 
+	 * @param array $tables
+	 * @return string html
+	 */
+	protected function renderMessage(array $tables) {
+		$nonUtf8Tables = array();
+		$output = '<table>';
+		$output .= '<tr><th>Table</th><th>Charset</th></tr>';
+		foreach ($tables as $table => $data) {
+			$output .= sprintf('<tr><td style="border-top: 1px solid black; font-weight: bold;">%s</td><td style="border-top: 1px solid black; background-color: %s; font-weight: bold;">%s</td></tr>', 
+				$data['Name'],
+				$data['charset'] == 'utf8' ? '#CDEACA' : '#FBB19B',
+				$data['charset']
+			);		
+			if ($data['charset'] != 'utf8') {
+				$nonUtf8Tables[] = $table;
+			}
+			
+			foreach ($data['field_charset'] as $field => $charset) {
+				$output .= sprintf('<tr><td style="padding-left: 30px;">%s</td><td style="background-color: %s;">%s</td></tr>', 
+					$field,
+					$charset == 'utf8' ? '#CDEACA' : '#FBB19B',
+					$charset
+				);
+				if ($charset != 'utf8') {
+					if (!in_array($table, $nonUtf8Tables)) {
+						$nonUtf8Tables[] = $table;
+					}
+				}
+			}	
+		}
+		$output .= '</table>';
+		
+		if (count($nonUtf8Tables)) {
+			$output .= '<h4>Convert statements for non utf8 tables (and tables containing non utf8 fields):</h4>';
+			$output .= '<pre>';
+			foreach ($nonUtf8Tables as $table) {
+				$output .= sprintf("ALTER TABLE `%s` CONVERT TO CHARACTER SET `utf8` COLLATE `utf8_general_ci`;".chr(10), $table);
+			}
+			$output .= '</pre>';
+		}
+		return $output;
+	}
+	
+}
+
+?>
