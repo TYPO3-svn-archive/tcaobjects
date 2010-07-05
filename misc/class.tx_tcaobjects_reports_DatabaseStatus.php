d7ef1ce85a9e0238e519dd446ae27fa588f2af1e
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-05 10:33:06 +0000

    ADD: check for database charset, some refactoring
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tcaobjects/trunk@35361 735d13b6-9817-0410-8766-e36946ffe9aa

--- misc/class.tx_tcaobjects_reports_DatabaseStatus.php
+++ misc/class.tx_tcaobjects_reports_DatabaseStatus.php
@@ -1,9 +1,31 @@
-<?php 
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Fabrizio Branca <typo3@fabrizio-branca.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/ 
 
 /**
  * Database status provider
  * 
- * @author Fabrizio Branca
+ * @author Fabrizio Branca <typo3@fabrizio-branca.de>
  * @since 2010-04-21
  */
 class tx_tcaobjects_reports_DatabaseStatus implements tx_reports_StatusProvider {
@@ -16,15 +38,22 @@ class tx_tcaobjects_reports_DatabaseStatus implements tx_reports_StatusProvider 
 	 */
 	public function getStatus() {
 		
-		$tables = $this->getTableData();
+		$databaseCharset = $this->getDatabaseCharset($this->getDatabaseCreateStatement(TYPO3_db));
 		
-		if ($this->allUtf8($tables)) {
+		$tables = $this->getTableData();
+		$notUtf8Tables = $this->extractNonUtf8Tables($tables);
+		
+		$message = '';
+		
+		if (count($notUtf8Tables) == 0 && $databaseCharset == 'utf8') {
 			$severity = tx_reports_reports_status_Status::OK;
-			$value = 'All tables and fields are utf8';
+			$value = 'Database, all tables and all fields are utf8 encoded';
 		} else {
 			$severity = tx_reports_reports_status_Status::WARNING;
-			$value = 'Some tables and/or fields are not utf8';
-			$message = $this->renderMessage($tables);
+			$value = 'Database, tables or fields are not utf8 encoded';
+				
+			$message .= $this->renderMessage($tables, $databaseCharset);
+			$message .= $this->renderAlterStatements($notUtf8Tables, $databaseCharset);
 		}
 		
 		$statuses = array(
@@ -47,54 +76,121 @@ class tx_tcaobjects_reports_DatabaseStatus implements tx_reports_StatusProvider 
 	protected function getTableData() {
 		$tables = $GLOBALS['TYPO3_DB']->admin_get_tables();
 		foreach ($tables as $table => &$data) {
-			$query = 'SHOW CREATE TABLE ' . $table;
-			$res = $GLOBALS['TYPO3_DB']->admin_query($query);
-			$create = $GLOBALS['TYPO3_DB']->sql_fetch_row($res);
-			$createStatement = $create[1];
-			$matches = array();
-			preg_match('/DEFAULT CHARSET=(\w+)/', $createStatement, $matches);
-			$data['charset'] = $matches[1];
-			
-			$fieldMatches = array();
-			preg_match_all('/`(\w+)`.*character set (\w+)/', $createStatement, $fieldMatches);
-			$data['field_charset'] = array();
-			foreach ($fieldMatches[0] as $key => $fieldMatch) {
-				$data['field_charset'][$fieldMatches[1][$key]] = $fieldMatches[2][$key]; 
-			}
+			$createStatement = $this->getTableCreateStatement($table);
+			$data['charset'] = $this->getTableCharset($createStatement);
+			$data['field_charset'] = $this->getFieldCharsets($createStatement);
 		}
-		
 		return $tables;
+	}
+	
+	/**
+	 * Get database create statement
+	 * 
+	 * @param string (optional) database name, be default the typo3 db will be used
+	 * @return string
+	 */
+	protected function getDatabaseCreateStatement($database) {
+		$query = 'SHOW CREATE DATABASE ' . $database;
+		$res = $GLOBALS['TYPO3_DB']->admin_query($query);
+		$create = $GLOBALS['TYPO3_DB']->sql_fetch_row($res);
+		return $create[1];
+	}
+	
+	/**
+	 * Get table create statement
+	 * 
+	 * @param string table name
+	 * @return string table create statement
+	 */
+	protected function getTableCreateStatement($tableName) {
+		$query = 'SHOW CREATE TABLE ' . $tableName;
+		$res = $GLOBALS['TYPO3_DB']->admin_query($query);
+		$create = $GLOBALS['TYPO3_DB']->sql_fetch_row($res);
+		return $create[1];
+	}
+	
+	/**
+	 * Get database charset
+	 * 
+	 * @param string database create statement
+	 * @return string charset
+	 */
+	protected function getDatabaseCharset($databaseCreateStatement) {
+		$matches = array();
+		preg_match('/`(\w+)`.*character set (\w+)/i', $databaseCreateStatement, $matches);
+		return $matches[2];
+	}
+	
+	/**
+	 * Get table charset
+	 * 
+	 * @param string table create statement
+	 * @return string table charset
+	 */
+	protected function getTableCharset($tableCreateStatement) {
+		$matches = array();
+		preg_match('/DEFAULT CHARSET=(\w+)/', $tableCreateStatement, $matches);
+		return $matches[1];
+	}
+	
+	/**
+	 * Get field charsets
+	 * 
+	 * @param string table create statement
+	 * @return array field charsets
+	 */
+	protected function getFieldCharsets($tableCreateStatement) {
+		$fieldCharSets = array();
+		$fieldMatches = array();
+		$data['field_charset'] = array();
+		preg_match_all('/`(\w+)`.*character set (\w+)/', $tableCreateStatement, $fieldMatches);
+		foreach ($fieldMatches[0] as $key => $fieldMatch) {
+			$fieldCharSets[$fieldMatches[1][$key]] = $fieldMatches[2][$key]; 
+		}
+		return $fieldCharSets;
 	}
 	
 	/**
 	 * Check if all tables are in utf8
 	 * 
 	 * @param array $tables
-	 * @return bool
+	 * @return array non utf8 tables or tables containing non utf8 fields
 	 */
-	protected function allUtf8(array $tables) {
+	protected function extractNonUtf8Tables(array $tables) {
+		$nonUtf8Tables = array();
 		foreach ($tables as $table => $data) {
 			if ($data['charset'] != 'utf8') {
-				return false;
+				$nonUtf8Tables[] = $table;
 			}		
 			foreach ($data['field_charset'] as $field => $charset) {
 				if ($charset != 'utf8') {
-					return false;
+					if (!in_array($table, $nonUtf8Tables)) {
+						$nonUtf8Tables[] = $table;
+					}
 				}	
 			}
 		}
-		return true;
+		return $nonUtf8Tables;
 	}
 	
 	/**
 	 * Render message
 	 * 
 	 * @param array $tables
-	 * @return string html
+	 * @return string HTML output
 	 */
-	protected function renderMessage(array $tables) {
-		$nonUtf8Tables = array();
+	protected function renderMessage(array $tables, $databaseCharset) {
 		$output = '<table>';
+			
+			// output database charset
+		$output .= '<tr><th>Database</th><th>Charset</th></tr>';
+		$output .= sprintf('<tr><td style="border-top: 1px solid black; font-weight: bold;">%s</td><td style="border-top: 1px solid black; background-color: %s; font-weight: bold;">%s</td></tr>', 
+			TYPO3_db,
+			$databaseCharset == 'utf8' ? '#CDEACA' : '#FBB19B',
+			$databaseCharset
+		);	
+		
+			// output table/field charsets		
 		$output .= '<tr><th>Table</th><th>Charset</th></tr>';
 		foreach ($tables as $table => $data) {
 			$output .= sprintf('<tr><td style="border-top: 1px solid black; font-weight: bold;">%s</td><td style="border-top: 1px solid black; background-color: %s; font-weight: bold;">%s</td></tr>', 
@@ -102,9 +198,6 @@ class tx_tcaobjects_reports_DatabaseStatus implements tx_reports_StatusProvider 
 				$data['charset'] == 'utf8' ? '#CDEACA' : '#FBB19B',
 				$data['charset']
 			);		
-			if ($data['charset'] != 'utf8') {
-				$nonUtf8Tables[] = $table;
-			}
 			
 			foreach ($data['field_charset'] as $field => $charset) {
 				$output .= sprintf('<tr><td style="padding-left: 30px;">%s</td><td style="background-color: %s;">%s</td></tr>', 
@@ -112,23 +205,30 @@ class tx_tcaobjects_reports_DatabaseStatus implements tx_reports_StatusProvider 
 					$charset == 'utf8' ? '#CDEACA' : '#FBB19B',
 					$charset
 				);
-				if ($charset != 'utf8') {
-					if (!in_array($table, $nonUtf8Tables)) {
-						$nonUtf8Tables[] = $table;
-					}
-				}
 			}	
 		}
 		$output .= '</table>';
+		return $output;
+	}
+	
+	/**
+	 * Render alter statements for database and tables
+	 * 
+	 * @param array $nonUtf8Tables
+	 * @param string $databaseCharset
+	 * @return string HTML output containing render statements
+	 */
+	protected function renderAlterStatements(array $nonUtf8Tables, $databaseCharset) {
+		$output = '<h4>Convert statements for non utf8 database and tables (and tables containing non utf8 fields):</h4>';
+		$output .= '<pre>';
 		
-		if (count($nonUtf8Tables)) {
-			$output .= '<h4>Convert statements for non utf8 tables (and tables containing non utf8 fields):</h4>';
-			$output .= '<pre>';
-			foreach ($nonUtf8Tables as $table) {
-				$output .= sprintf("ALTER TABLE `%s` CONVERT TO CHARACTER SET `utf8` COLLATE `utf8_general_ci`;".chr(10), $table);
-			}
-			$output .= '</pre>';
+		if ($databaseCharset != 'utf8') {
+			$output .= sprintf("ALTER DATABASE `%s` CHARACTER SET `utf8` COLLATE `utf8_general_ci`;".chr(10).chr(10), TYPO3_db);
 		}
+		foreach ($nonUtf8Tables as $table) {
+			$output .= sprintf("ALTER TABLE `%s` CONVERT TO CHARACTER SET `utf8` COLLATE `utf8_general_ci`;".chr(10), $table);
+		}
+		$output .= '</pre>';
 		return $output;
 	}
 	
